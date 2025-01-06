e343277ae3f396679155682efc364f1149011196
asierra <algsierra@gmail.com>
2025-01-05 18:52:10 -0600

    Initial builds

---
+++ script.js
@@ -0,0 +1,13 @@
+const searchButton = document.getElementById("btnSearch");
+const clearButton = document.getElementById("btnClear");
+
+function findDestination(keyword) {
+    const foundEmployee = employees.find(employee => employee.id === employeeId);
+    if (foundEmployee) {
+    document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+    }
+    else{
+      document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+
+    }
+}
